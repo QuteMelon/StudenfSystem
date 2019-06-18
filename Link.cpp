9f162dc477515ef2f5869f2e244d1ab54f6e2dfc
QuteMelon <44717348+QuteMelon@users.noreply.github.com>
2019-06-18 19:31:38 +0800

    Create Link.cpp

---
+++ Link.cpp
@@ -0,0 +1,3 @@
+QT比例缩放：https://blog.csdn.net/sinat_36420785/article/details/61432531
+QT打包程序:https://jingyan.baidu.com/article/af9f5a2d60997343140a45b3.html
+QT信号槽按钮：https://www.cnblogs.com/felix-wang/p/6212197.html
